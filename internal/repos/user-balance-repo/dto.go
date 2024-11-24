8ba89159cb888a342c1fec43ef9fac378281b0c4
aridae <rusinovadaria185@gmail.com>
2024-11-24 23:12:37 +0300

    buggo-boo

--- internal/repos/user-balance-repo/dto.go
+++ internal/repos/user-balance-repo/dto.go
@@ -1,19 +1,23 @@
 package userbalancerepo
 
-import "github.com/aridae/gophermart-diploma/internal/model"
+import (
+	"github.com/aridae/gophermart-diploma/internal/model"
+	"github.com/aridae/gophermart-diploma/pkg/pointer"
+)
 
 type balanceDTO struct {
 	UserLogin           string `db:"user_login"`
 	CurrentBalanceCents int64  `db:"current_balance_cents"`
-	WithdrawnCents      int64  `db:"withdrawn_cents"`
+	WithdrawnCents      *int64 `db:"withdrawn_cents"`
 }
 
 func mapDTOToDomainUserBalance(dto balanceDTO) model.Balance {
+	withdrawnCents := pointer.Deref(dto.WithdrawnCents, 0)
 	return model.Balance{
 		Owner: model.User{
 			Login: dto.UserLogin,
 		},
 		Current:   model.NewMoneyFromCents(dto.CurrentBalanceCents),
-		Withdrawn: model.NewMoneyFromCents(dto.WithdrawnCents),
+		Withdrawn: model.NewMoneyFromCents(withdrawnCents),
 	}
 }
